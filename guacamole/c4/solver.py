7e05dc959f52022e19bce33fabd18897a0f8e7ed
Nicolas Primeau <nprimeau@reverso.com>
2019-08-19 17:45:03 -0400

    k

--- guacamole/c4/solver.py
+++ guacamole/c4/solver.py
@@ -20,7 +20,7 @@ class GameSolver:
             self.print_stats()
 
     def print_stats(self):
-        if self._game_plays % 1000 == 0:
+        if self._game_plays % 500 == 0:
             print(datetime.datetime.now())
             print('P1 win rate: {}'.format(self._won_counts[0] / self._game_plays))
             print('P2 win rate: {}'.format(self._won_counts[1] / self._game_plays))
