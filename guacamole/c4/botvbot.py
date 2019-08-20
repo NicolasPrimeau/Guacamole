24107cdc2a01d48bdbacad3c26ded9cef008218d
Nicolas Primeau <nprimeau@reverso.com>
2019-08-20 16:14:01 -0400

    k

--- guacamole/c4/botvbot.py
+++ guacamole/c4/botvbot.py
@@ -6,9 +6,13 @@ from guacamole.c4.solver import GameSolver
 def main():
     controller = GameStateController()
     player1 = QClient(controller, GameToken.PLAYER1, save_path="states/player1.dat")
-    player1.load()
+    print('Loading player 1')
+    if not player1.load():
+        print('Failed to load player 1!')
     player2 = QClient(controller, GameToken.PLAYER2, save_path="states/player2.dat")
-    player2.load()
+    print('Loading player 2')
+    if not player2.load():
+        print('Failed to load player 2!')
     solver = GameSolver(controller, [player1, player2], lambda: False)
     solver.solve()
 
