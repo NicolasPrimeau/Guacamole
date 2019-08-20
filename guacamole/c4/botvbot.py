b53dbf83b4bde4aaf9a5fa293f95663ef3fd1845
Nicolas Primeau <nprimeau@reverso.com>
2019-08-20 10:44:31 -0400

    k

--- guacamole/c4/botvbot.py
+++ guacamole/c4/botvbot.py
@@ -5,9 +5,9 @@ from guacamole.c4.solver import GameSolver
 
 def main():
     controller = GameStateController()
-    player1 = QClient(controller, GameToken.PLAYER1, save_path="states/player1.json")
+    player1 = QClient(controller, GameToken.PLAYER1, save_path="states/player1.dat")
     player1.load()
-    player2 = QClient(controller, GameToken.PLAYER2, save_path="states/player2.json")
+    player2 = QClient(controller, GameToken.PLAYER2, save_path="states/player2.dat")
     player2.load()
     solver = GameSolver(controller, [player1, player2], lambda: False)
     solver.solve()
--- guacamole/c4/pvbot.py
+++ guacamole/c4/pvbot.py
@@ -7,7 +7,7 @@ from guacamole.c4.solver import GameSolver
 def main():
     controller = GameStateController()
     player1 = InputClient(controller, GameToken.PLAYER1)
-    player2 = QClient(controller, GameToken.PLAYER2, save_path="states/player2.json")
+    player2 = QClient(controller, GameToken.PLAYER2, save_path="states/player2.dat")
     player2.load()
     solver = GameSolver(controller, [player1, player2], lambda: False)
     solver.solve()
